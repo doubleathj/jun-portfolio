374ae985bcc824797ca52522fae23fba629e4993
jun <doubleat.hj@gmail.com>
2023-04-23 15:27:59 +0900

    Feat: 추가 next.config Redirect
    / -> /resume 리다이렉트 처리 (307)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/resume",
+        permanent: false, // 307 redirect
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
